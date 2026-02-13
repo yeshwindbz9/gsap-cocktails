87cad11924bea154ba1257192cd78c7046eae0af
yeshwindbz9 <yeshwindbz9@gmail.com>
2026-02-13 10:29:01 +0000

    updated $ to £

--- constants/index.js
+++ constants/index.js
@@ -22,25 +22,25 @@ const cocktailLists = [
     name: "Chapel Hill Shiraz",
     country: "AU",
     detail: "Battle",
-    price: "$10",
+    price: "£10",
   },
   {
     name: "Caten Malbee",
     country: "AU",
     detail: "Battle",
-    price: "$49",
+    price: "£49",
   },
   {
     name: "Rhino Pale Ale",
     country: "CA",
     detail: "750 ml",
-    price: "$20",
+    price: "£20",
   },
   {
     name: "Irish Guinness",
     country: "IE",
     detail: "600 ml",
-    price: "$29",
+    price: "£29",
   },
 ];
 
@@ -49,25 +49,25 @@ const mockTailLists = [
     name: "Tropical Bloom",
     country: "US",
     detail: "Battle",
-    price: "$10",
+    price: "£10",
   },
   {
     name: "Passionfruit Mint",
     country: "US",
     detail: "Battle",
-    price: "$49",
+    price: "£49",
   },
   {
     name: "Citrus Glow",
     country: "CA",
     detail: "750 ml",
-    price: "$20",
+    price: "£20",
   },
   {
     name: "Lavender Fizz",
     country: "IE",
     detail: "600 ml",
-    price: "$29",
+    price: "£29",
   },
 ];
 
